5426c3fcd6de2e5c6d4c862469d76d19c306d22f
닮 <dhdpekfa1@daum.net>
2024-11-12 17:37:01 +0900

    [init] 초기 설정

---
+++ README.md
@@ -0,0 +1,5 @@
+## 날씨 오픈 API를 활용한 대시보드 만들기
+
+<!-- UI는 shadcn.ui 사용 -->
+
+1. 리액트 라우터 설치: `npm install react-router-dom`
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="page">
+      <div className="page__container"></div>
+    </div>
+  );
+};
+
+export default Home;
