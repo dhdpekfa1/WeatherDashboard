cd23a2f84a71a2cd85afefb3363624fe5b2bb5b4
닮 <dhdpekfa1@daum.net>
2024-11-13 11:05:26 +0900

    [feat] assets/icons 폴더에 아이콘 파일 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,12 @@ const Home = () => {
     <div className="page">
       <div className="page__container">
         <Header />
+        <div className="w-full h-full">
+          {/* 상단 3개 위젯 1 2 1 */}
+          <div className="w-full flex items-center gap-6 bg-slate-500"></div>
+          {/* 상단 2개 위젯 3 1 */}
+          <div className="w-full flex items-center gap-6 bg-orange-500"></div>
+        </div>
       </div>
     </div>
   );
