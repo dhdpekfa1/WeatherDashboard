6bfecc26258280e405f2615429c7f621a51bbb1c
닮 <dhdpekfa1@daum.net>
2024-11-14 14:42:47 +0900

    [feat] KakaoMapCard에 마커 추가

--- src/components/home/KakaoMapCard.tsx
+++ src/components/home/KakaoMapCard.tsx
@@ -1,9 +1,44 @@
 import useKakaoLoader from "@/hooks/useKakaoLoader";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { Card } from "@/components";
 
 const KakaoMapCard = () => {
   useKakaoLoader();
+
+  const positions = [
+    {
+      cityName: "seoul",
+      latlng: { lat: 37.5683, lng: 126.9778 },
+    },
+    {
+      cityName: "incheon",
+      latlng: { lat: 37.4562557, lng: 126.7052062 },
+    },
+    {
+      cityName: "gwangju",
+      latlng: { lat: 35.1599785, lng: 126.8513072 },
+    },
+    {
+      cityName: "daejeon",
+      latlng: { lat: 36.3504567, lng: 127.3848187 },
+    },
+    {
+      cityName: "cheongju",
+      latlng: { lat: 36.6358093, lng: 127.4913338 },
+    },
+    {
+      cityName: "daegu",
+      latlng: { lat: 35.8715411, lng: 128.601505 },
+    },
+    {
+      cityName: "ulsan",
+      latlng: { lat: 35.5396224, lng: 129.3115276 },
+    },
+    {
+      cityName: "busan",
+      latlng: { lat: 35.179665, lng: 129.0747635 },
+    },
+  ];
 
   return (
     <Card className="w-1/4 min-w-[25%] h-full">
@@ -21,7 +56,22 @@ const KakaoMapCard = () => {
           borderRadius: "8px",
         }}
         level={12} // 지도의 확대 레벨
-      />
+      >
+        {positions.map((position) => (
+          <MapMarker
+            key={`${position.cityName}-${position.latlng}`}
+            position={position.latlng}
+            image={{
+              src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+              size: {
+                width: 24,
+                height: 35,
+              },
+            }}
+            title={position.cityName}
+          />
+        ))}
+      </Map>
     </Card>
   );
 };
